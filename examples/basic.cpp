39296a7b6352243ca304adab4cc95d180b7dcea8
Stefan Hagen <stefan@hagen.link>
2021-09-15 08:35:55 +0200

    Update basic.cpp

--- examples/basic.cpp
+++ examples/basic.cpp
@@ -1,1 +1,22 @@
+#include <iostream>
 
+#include <toml++/toml.h>
+using namespace std::string_view_literals;
+
+int main() {
+    static constexpr std::string_view configuration = R"(
+        [library]
+        name = "toml++"
+        authors = ["Mark"]
+        cpp = 17
+    )"sv;
+    
+    try {
+        toml::table tbl = toml::parse(configuration);
+        std::cout << tbl << "\n";
+    }
+    catch (const toml::parse_error& err) {
+        std::cerr << "Parsing failed:\n" << err << "\n";
+        return 1;
+    }
+}
