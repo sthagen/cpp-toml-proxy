4df2486d5a27601e603032d23d7ce19e876eb7ec
Stefan Hagen <stefan@hagen.link>
2021-09-15 08:35:27 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,36 @@
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 
+#include <array>
+#include <string>
+#include <sstream>
+
+#include <doctest/doctest.h>
+
+#include <toml++/toml.h>
+using namespace std::string_view_literals;
+
+
+TEST_SUITE ("Example derived tests.") {
+    TEST_CASE ("No error in parsing the example report.") {
+        /* Example snippet:
+         *
+         * TODO(sthagen) fill in the example liaison info
+         */
+        static constexpr std::string_view configuration = R"(
+            [library]
+            name = "toml++"
+            authors = ["Mark"]
+            cpp = 17
+        )"sv;
+
+        try {
+            toml::table tbl = toml::parse(configuration);
+            std::cout << tbl << "\n";
+        }
+        catch (const toml::parse_error& err) {
+            std::cerr << "Parsing failed:\n" << err << "\n";
+            return 1;
+        }
+            REQUIRE(tbl);
+    }
+}
