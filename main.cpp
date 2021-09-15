787bafe4463832bc9c03c22d546af9842ddc492b
Stefan Hagen <stefan@hagen.link>
2021-09-15 08:43:58 +0200

    Adjusted copy-pasta

--- main.cpp
+++ main.cpp
@@ -9,28 +9,26 @@
 #include <toml++/toml.h>
 using namespace std::string_view_literals;
 
+static constexpr std::string_view configuration = R"(
+    [library]
+    name = "toml++"
+    authors = ["Mark"]
+    cpp = 17
+)"sv;
+
 
 TEST_SUITE ("Example derived tests.") {
     TEST_CASE ("No error in parsing the example report.") {
         /* Example snippet:
          *
-         * TODO(sthagen) fill in the example liaison info
+         * toml::table tbl = toml::parse(configuration);
          */
-        static constexpr std::string_view configuration = R"(
-            [library]
-            name = "toml++"
-            authors = ["Mark"]
-            cpp = 17
-        )"sv;
-
         try {
             toml::table tbl = toml::parse(configuration);
-            std::cout << tbl << "\n";
+            REQUIRE(tbl);
         }
         catch (const toml::parse_error& err) {
-            std::cerr << "Parsing failed:\n" << err << "\n";
-            return 1;
+            FAIL("Parsing failed:", err);
         }
-            REQUIRE(tbl);
     }
 }
